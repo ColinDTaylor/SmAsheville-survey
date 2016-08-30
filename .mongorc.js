e9773d3915d3c466c2c2e4f5b9fd16453f2b7fa4
Absolome <absolome@gmail.com>
2016-08-29 22:15:42 -0400

    added a mongorc.js

---
+++ .mongorc.js
@@ -0,0 +1,24 @@
+/* Future to do:            */
+/*            */
+/*            */
+/*            */
+/*            */
+function surveySubmitters(coll) {
+  return coll.find({}, {_id: 0, tag: 1}).toArray()
+}
+
+function testFunction(coll) {
+  return coll.find({}, {_id: 0, name: 1})
+}
+
+function getAttendance(coll) {
+  return coll.aggregate([
+    { $group: { _id: '$name', attendance: {$sum: 1} } },
+    { $sort: {attendance: -1} }
+  ])
+}
+
+function handleLists(coll) {
+  let listObject = coll.find({}, {_id: 0, tag: 1, pr_list: 1, unranked_players: 1})
+  return listObject
+}
