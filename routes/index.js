3ef796370630bdb375bffa1487a396dc27e2cb4c
Absolome <absolome@gmail.com>
2016-11-08 16:36:02 -0500

    updated index

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var challongeApi = require('../js/api-basics.js');
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'SURPRISE: it\'s nothing',
                         test:   'Im gay'
                         // test:   challongeApi(function(data){
                         //     toString((data))
                         // })
-                    });
-});
+                    })
+})
 
-module.exports = router;
+module.exports = router
